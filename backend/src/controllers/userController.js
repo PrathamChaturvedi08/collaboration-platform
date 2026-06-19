9bc1660ec583dda89ea5b20d2c6a0297b1bc64bd
Pratham <prathamc.0801@gmail.com>
2026-06-19 19:07:04 +0530

    Refactor to MVC architecture

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require("../models/User");
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,8 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const dotenv = require("dotenv");
+
 const connectDB = require("./config/db");
-const User = require("./models/User");
 
 dotenv.config();
 
@@ -18,22 +18,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
+app.use("/api/users", require("./routes/userRoutes"));
+
 const PORT = process.env.PORT || 5000;
-
-app.post("/api/users", async (req, res) => {
-  try {
-    const user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-    });
-
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
