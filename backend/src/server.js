d8320f7c60b17c89ca3e31e61cc284439087c0a5
Pratham <prathamc.0801@gmail.com>
2026-06-19 17:43:59 +0530

    Add POST route and middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const app = express();
 
+/*Middleware*/
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello Pratham");
+  res.send("Backend is running");
+});
+
+app.get("/api/users", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Pratham",
+    },
+    {
+      id: 2,
+      name: "Rahul",
+    },
+  ]);
+});
+
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    success: true,
+    receivedData: req.body,
+  });
 });
 
 app.listen(5000, () => {
